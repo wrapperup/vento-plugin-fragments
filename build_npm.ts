55e98fa094d00b61ff9154cf8069cc8e358e3d42
wrapperup <wrapperup4@gmail.com>
2024-02-22 03:37:59 -0500

    init commit

---
+++ build_npm.ts
@@ -0,0 +1,39 @@
+// ex. scripts/build_npm.ts
+import { build, emptyDir } from "https://deno.land/x/dnt/mod.ts";
+
+await emptyDir("./npm");
+
+await build({
+  entryPoints: ["./mod.ts"],
+  outDir: "./npm",
+  shims: {
+    // see JS docs for overview and more options
+    deno: true,
+  },
+  package: {
+    // package.json properties
+    name: "your-package",
+    version: Deno.args[0],
+    description: "Your package.",
+    license: "MIT",
+    repository: {
+      type: "git",
+      url: "git+https://github.com/username/repo.git",
+    },
+    bugs: {
+      url: "https://github.com/username/repo/issues",
+    },
+  },
+  mappings: {
+    "ventojs": {
+      name: "ventojs",
+      version: "^0.11.0",
+      peerDependency: true,
+    },
+  },
+  postBuild() {
+    // steps to run after building and before running the tests
+    Deno.copyFileSync("LICENSE", "npm/LICENSE");
+    Deno.copyFileSync("README.md", "npm/README.md");
+  },
+});
---
+++ src/mod.ts
@@ -0,0 +1,58 @@
+import { Environment } from "ventojs/src/environment.ts";
+import { Token } from "ventojs/src/tokenizer.ts";
+
+function fragmentPreprocessor(
+  _env: Environment,
+  tokens: Token[],
+  path?: string,
+): Token[] | undefined {
+  if (!path || !path.includes("#")) {
+    // Filter out all fragment tags
+    return tokens.filter((token) =>
+      token[0] !== "tag" || !token[1].startsWith("fragment "),
+    );
+  }
+
+  const splitPath = path.split("#");
+
+  if (splitPath.length !== 2) {
+    throw new Error("Fragment query has multiple \"'#\" characters: " + path);
+  }
+
+  const [_, fragment] = splitPath;
+
+  let indexStart = 0;
+  while (indexStart < tokens.length) {
+    const token = tokens[indexStart];
+
+    if (token[0] === "tag" && token[1] === "fragment " + fragment) {
+      // Found fragment, remove it and return the rest of the tokens
+      indexStart++;
+      break;
+    }
+    indexStart++;
+  }
+
+  if (indexStart >= tokens.length) {
+    throw new Error("Couldn't find fragment \"" + fragment + '"');
+  }
+
+  let indexEnd = indexStart;
+
+  while (indexEnd < tokens.length) {
+    const token = tokens[indexEnd];
+    if (token[0] === "tag" && token[1] === "/fragment") {
+      const fragment = tokens.slice(indexStart, indexEnd);
+      return fragment;
+    }
+    indexEnd++;
+  }
+
+  throw new Error("Couldn't find end of fragment \"" + fragment + '"');
+}
+
+export function fragmentPlugin() {
+  return (env: Environment) => {
+    env.tokenPreprocessors.push(fragmentPreprocessor);
+  };
+}
