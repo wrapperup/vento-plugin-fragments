3b8fae4836851be73da12a552e9d6212544c75bb
wrapperup <wrapperup4@gmail.com>
2024-03-10 20:51:00 -0400

    add tests, add string token merging

--- src/mod.ts
+++ src/mod.ts
@@ -1,5 +1,32 @@
 import { Environment, Token } from "../deps.ts";
 
+// Re-merge string tokens that were split up by fragment tags
+function mergeStringTokens(tokens: Token[]) {
+  let i = 0;
+  while (i < tokens.length) {
+    if (tokens[i][0] === "string") {
+      const j = i + 1;
+      while (j < tokens.length && tokens[j][0] === "string") {
+        tokens[i][1] += tokens[j][1];
+        tokens.splice(j, 1);
+      }
+    }
+    i++;
+  }
+}
+
+function filterFragments(tokens: Token[]) {
+  const filtered = tokens.filter((token) =>
+    token[0] !== "tag" ||
+    (!token[1].trim().startsWith("fragment ") &&
+      token[1].trim() !== "/fragment")
+  );
+
+  mergeStringTokens(filtered);
+
+  return filtered;
+}
+
 function fragmentPreprocessor(
   _env: Environment,
   tokens: Token[],
@@ -7,15 +34,13 @@ function fragmentPreprocessor(
 ): Token[] | undefined {
   if (!path || !path.includes("#")) {
     // Filter out all fragment tags
-    return tokens.filter((token) =>
-      token[0] !== "tag" || !token[1].startsWith("fragment "),
-    );
+    return filterFragments(tokens);
   }
 
   const splitPath = path.split("#");
 
   if (splitPath.length !== 2) {
-    throw new Error("Fragment query has multiple \"'#\" characters: " + path);
+    throw new Error('Fragment query has multiple "#" characters: ' + path);
   }
 
   const [_, fragment] = splitPath;
@@ -38,11 +63,17 @@ function fragmentPreprocessor(
 
   let indexEnd = indexStart;
 
+  let depth = 0;
   while (indexEnd < tokens.length) {
     const token = tokens[indexEnd];
-    if (token[0] === "tag" && token[1] === "/fragment") {
+    if (token[0] === "tag" && token[1].startsWith("fragment ")) {
+      depth++;
+    } else if (token[0] === "tag" && token[1] === "/fragment") {
+      depth--;
+    }
+    if (depth <= -1) {
       const fragment = tokens.slice(indexStart, indexEnd);
-      return fragment;
+      return filterFragments(fragment);
     }
     indexEnd++;
   }
@@ -50,7 +81,7 @@ function fragmentPreprocessor(
   throw new Error("Couldn't find end of fragment \"" + fragment + '"');
 }
 
-export function fragmentPlugin() {
+export default function fragments() {
   return (env: Environment) => {
     env.tokenPreprocessors.push(fragmentPreprocessor);
   };
---
+++ test/fragment.test.ts
@@ -0,0 +1,217 @@
+import tmpl from "https://deno.land/x/vento@v0.12.1/mod.ts";
+import fragments from "../src/mod.ts";
+import { assertEquals } from "https://deno.land/std@0.205.0/assert/assert_equals.ts";
+
+Deno.test("Simple fragment", () => {
+  const code = `
+    <h1>Hello!</h1>
+    {{ fragment first }}<p>First fragment</p>{{ /fragment }}
+
+    {{ description }}
+    `;
+
+  const env = tmpl();
+  env.use(fragments());
+
+  const tokens = env.tokenize(code, "test.vto#first");
+  assertEquals(tokens, [
+    ["string", "<p>First fragment</p>", 45],
+  ]);
+});
+
+Deno.test("Double nested fragment", () => {
+  const code = `
+    <h1>Hello!</h1>
+    {{ fragment first }}
+      <p>First fragment</p>
+      {{ fragment second }}<p>Second fragment</p>{{ /fragment }}
+      <p>Bottom of first fragment</p>
+    {{ /fragment }}
+
+    {{ description }}
+    `;
+
+  const env = tmpl();
+  env.use(fragments());
+
+  {
+    const tokens = env.tokenize(code, "test.vto#first");
+    assertEquals(tokens, [
+      [
+        "string",
+        "\n" +
+        "      <p>First fragment</p>\n" +
+        "      <p>Second fragment</p>\n" +
+        "      <p>Bottom of first fragment</p>\n" +
+        "    ",
+        45,
+      ],
+    ]);
+  }
+
+  {
+    const tokens = env.tokenize(code, "test.vto#second");
+    assertEquals(tokens, [
+      [
+        "string",
+        "<p>Second fragment</p>",
+        101,
+      ],
+    ]);
+  }
+});
+
+Deno.test("Complex nested fragment", () => {
+  const code = `<h1>Hello!</h1>
+    {{- fragment first -}}
+      <p>First fragment</p>
+      {{- fragment second }}<p>Second fragment</p>{{ /fragment -}}
+      {{- fragment third -}}
+        <p>Third fragment</p>
+        {{- fragment fourth -}}<p>Fourth fragment</p>{{- /fragment -}}
+      {{- /fragment -}}
+      <p>Bottom of first fragment</p>
+    {{- /fragment }}
+    `;
+
+  const env = tmpl();
+  env.use(fragments());
+
+  {
+    const tokens = env.tokenize(code, "test.vto#first");
+    assertEquals(tokens, [
+      [
+        "string",
+        "<p>First fragment</p><p>Second fragment</p><p>Third fragment</p><p>Fourth fragment</p><p>Bottom of first fragment</p>",
+        42,
+      ],
+    ]);
+  }
+  {
+    const tokens = env.tokenize(code, "test.vto#second");
+    assertEquals(tokens, [
+      [
+        "string",
+        "<p>Second fragment</p>",
+        99,
+      ],
+    ]);
+  }
+  {
+    const tokens = env.tokenize(code, "test.vto#third");
+    assertEquals(tokens, [
+      [
+        "string",
+        "<p>Third fragment</p><p>Fourth fragment</p>",
+        166,
+      ],
+    ]);
+  }
+  {
+    const tokens = env.tokenize(code, "test.vto#fourth");
+    assertEquals(tokens, [
+      [
+        "string",
+        "<p>Fourth fragment</p>",
+        228,
+      ],
+    ]);
+  }
+});
+
+Deno.test("Fragment with data", () => {
+  const code = `
+    <h1>Hello!</h1>
+    {{ fragment first }}<p>{{ name }}</p>{{ /fragment }}
+
+    {{ description }}
+    `;
+
+  const env = tmpl();
+  env.use(fragments());
+
+  const tokens = env.tokenize(code, "test.vto#first");
+
+  assertEquals(tokens, [
+    [
+      "string",
+      "<p>",
+      45,
+    ],
+    [
+      "tag",
+      "name",
+      48,
+    ],
+    [
+      "string",
+      "</p>",
+      58,
+    ],
+  ]);
+});
+
+Deno.test("Nested fragment with data", () => {
+  const code = `
+    <h1>Hello!</h1>
+    {{ fragment first }}<span>{{ name }}</span>{{ fragment second }}<p>{{ description }}</p>{{ /fragment }}{{ /fragment }}
+
+    {{ description }}
+    `;
+
+  const env = tmpl();
+  env.use(fragments());
+
+  {
+    const tokens = env.tokenize(code, "test.vto#first");
+
+    assertEquals(tokens, [
+      [
+        "string",
+        "<span>",
+        45,
+      ],
+      [
+        "tag",
+        "name",
+        51,
+      ],
+      [
+        "string",
+        "</span><p>",
+        61,
+      ],
+      [
+        "tag",
+        "description",
+        92,
+      ],
+      [
+        "string",
+        "</p>",
+        109,
+      ],
+    ]);
+  }
+  {
+    const tokens = env.tokenize(code, "test.vto#second");
+
+    assertEquals(tokens, [
+      [
+        "string",
+        "<p>",
+        89,
+      ],
+      [
+        "tag",
+        "description",
+        92,
+      ],
+      [
+        "string",
+        "</p>",
+        109,
+      ],
+    ]);
+  }
+});
